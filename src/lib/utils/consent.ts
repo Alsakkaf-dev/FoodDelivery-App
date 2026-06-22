4b52bd2514885bd137cf0e64ea842c53a69ff1a0
Alsakkaf-dev <mohammed.alsakkaaf@gmail.com>
2026-06-22 22:12:25 +0800

    consent: add CONSENT_VERSION policy constant

---
+++ src/lib/utils/consent.ts
@@ -0,0 +1,3 @@
+// PDPA privacy-policy version the user consents to at sign-in (US-007). Bump on
+// any change to the data-use / notification consent copy so re-consent is tracked.
+export const CONSENT_VERSION = '1.0';
