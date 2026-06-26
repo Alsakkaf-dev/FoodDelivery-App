137776b0e8140adf9d343800affc7f391ca65f9f
Alsakkaf-dev <mohammed.alsakkaaf@gmail.com>
2026-06-26 19:16:07 +0800

    icons: add meta.tsx (part 2/5)

--- src/components/icons/meta.tsx
+++ src/components/icons/meta.tsx
@@ -48,3 +48,50 @@ export function MapPinIcon(props: IconProps) {
     </IconBase>
   );
 }
+
+export function NavigationIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M21 3 3 10.6l7.8 2.6L13.4 21Z" />
+    </IconBase>
+  );
+}
+
+export function WalletIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M4 7.5A2 2 0 0 1 6 5.5h11A1.5 1.5 0 0 1 18.5 7H6" />
+      <path d="M4 7.5V18a2 2 0 0 0 2 2h12a1.5 1.5 0 0 0 1.5-1.5V11A1.5 1.5 0 0 0 18 9.5H6" />
+      <circle cx="16.5" cy="14.5" r="1.2" />
+    </IconBase>
+  );
+}
+
+export function CreditCardIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <rect x="3" y="6" width="18" height="12" rx="2.5" />
+      <path d="M3 10h18" />
+      <path d="M7 14.5h3" />
+    </IconBase>
+  );
+}
+
+export function BellIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M6.5 10a5.5 5.5 0 0 1 11 0c0 5 2 6.5 2 6.5H4.5s2-1.5 2-6.5Z" />
+      <path d="M10 19.5a2.2 2.2 0 0 0 4 0" />
+    </IconBase>
+  );
+}
+
+export function MailIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <rect x="3" y="5.5" width="18" height="13" rx="2.5" />
+      <path d="m4 7.5 8 5.5 8-5.5" />
+    </IconBase>
+  );
+}
+
