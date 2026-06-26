a8db1d72dedc95cbd01223fefce20d17270fce04
Alsakkaf-dev <mohammed.alsakkaaf@gmail.com>
2026-06-26 19:16:05 +0800

    icons: add actions.tsx (part 2/5)

--- src/components/icons/actions.tsx
+++ src/components/icons/actions.tsx
@@ -39,3 +39,52 @@ export function CloseIcon(props: IconProps) {
   );
 }
 
+export function TrashIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M4 7h16" />
+      <path d="M9 7V5.5A1.5 1.5 0 0 1 10.5 4h3A1.5 1.5 0 0 1 15 5.5V7" />
+      <path d="M6.5 7l1 12.5a1.5 1.5 0 0 0 1.5 1.4h6a1.5 1.5 0 0 0 1.5-1.4l1-12.5" />
+      <path d="M10 11v6M14 11v6" />
+    </IconBase>
+  );
+}
+
+export function EditIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M4 20l1-4L16 5a2 2 0 0 1 3 3L8 19Z" />
+      <path d="m13.5 6.5 4 4" />
+    </IconBase>
+  );
+}
+
+export function ShareIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <circle cx="6" cy="12" r="2.5" />
+      <circle cx="18" cy="6" r="2.5" />
+      <circle cx="18" cy="18" r="2.5" />
+      <path d="m8.2 10.8 7.6-3.6M8.2 13.2l7.6 3.6" />
+    </IconBase>
+  );
+}
+
+export function FilterIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M4 5h16l-6 7v6l-4 2v-8Z" />
+    </IconBase>
+  );
+}
+
+export function SlidersIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M4 7h10M18 7h2M4 17h6M14 17h6" />
+      <circle cx="16" cy="7" r="2.2" />
+      <circle cx="12" cy="17" r="2.2" />
+    </IconBase>
+  );
+}
+
