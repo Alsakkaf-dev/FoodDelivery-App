cff6254e1c1fff201c5262ba677df82a8cc50101
Alsakkaf-dev <mohammed.alsakkaaf@gmail.com>
2026-06-26 19:16:06 +0800

    icons: add actions.tsx (part 3/5)

--- src/components/icons/actions.tsx
+++ src/components/icons/actions.tsx
@@ -88,3 +88,52 @@ export function SlidersIcon(props: IconProps) {
   );
 }
 
+export function CameraIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M4 8.5h3l1.5-2.2h7L17 8.5h3a1 1 0 0 1 1 1V18a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.5a1 1 0 0 1 1-1Z" />
+      <circle cx="12" cy="13" r="3.2" />
+    </IconBase>
+  );
+}
+
+export function UploadIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M12 15.5V4" />
+      <path d="m7.5 8.5 4.5-4.5 4.5 4.5" />
+      <path d="M5 16v2.5a1.5 1.5 0 0 0 1.5 1.5h11a1.5 1.5 0 0 0 1.5-1.5V16" />
+    </IconBase>
+  );
+}
+
+export function CloudUploadIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M7 18a4 4 0 0 1-.5-7.97A5.5 5.5 0 0 1 17 9.5a3.5 3.5 0 0 1 .5 6.96" />
+      <path d="M12 21v-7" />
+      <path d="m9 16 3-3 3 3" />
+    </IconBase>
+  );
+}
+
+export function EyeIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M2.5 12S6 5.5 12 5.5 21.5 12 21.5 12 18 18.5 12 18.5 2.5 12 2.5 12Z" />
+      <circle cx="12" cy="12" r="3" />
+    </IconBase>
+  );
+}
+
+export function EyeOffIcon(props: IconProps) {
+  return (
+    <IconBase {...props}>
+      <path d="M4 4 20 20" />
+      <path d="M9.55 9.6A3 3 0 0 0 14.4 14.45" />
+      <path d="M6.5 6.6C4.2 8 2.5 12 2.5 12s3.5 6.5 9.5 6.5a9 9 0 0 0 3.6-.75" />
+      <path d="M9.8 5.7A9.6 9.6 0 0 1 12 5.5c6 0 9.5 6.5 9.5 6.5a16.5 16.5 0 0 1-2.45 3.15" />
+    </IconBase>
+  );
+}
+
